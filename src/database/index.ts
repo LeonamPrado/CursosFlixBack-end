ec2b482efcdbd950cce764e4d604c8ae5afe613f
LeonamPrado <leonamopprado@gmail.com>
2024-01-28 16:58:59 -0300

    update

--- src/database/index.ts
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from 'sequelize'
-import { DATABASE_URL } from '../config/environment'
-
-export const database = new Sequelize(DATABASE_URL,{
-	define: {
-    underscored: true
-    }
-})
